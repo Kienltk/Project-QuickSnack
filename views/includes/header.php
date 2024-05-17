5a903590ea5ad646b120e62ba7ab820ad29c74fe
nguyen dinh chien <Ndc7571@gmail.com>
2024-05-17 12:27:03 +0700

    complete project

--- views/includes/header.php
+++ views/includes/header.php
@@ -76,7 +76,7 @@ foreach ($pages_to_highlight as $page) {
         }
     }
 }
-include("../../database/connect_database/index.php");
+include ("../../database/connect_database/index.php");
 
 $user_id = isset($_COOKIE["userID"]) ? $_COOKIE["userID"] : null;
 if ($user_id) {
@@ -96,22 +96,27 @@ if ($user_id) {
         <a class="navbar-brand ms-3" href="../home/home.php">
             <img src="../../public/image/logo.png" alt="" width="100" height="100">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto fs-5">
                 <li class="nav-item">
-                    <a class="nav-link mx-3 navbar_text <?php echo $category_class; ?>" href="../products/category.php">Category</a>
+                    <a class="nav-link mx-3 navbar_text <?php echo $category_class; ?>"
+                        href="../products/category.php">Category</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-3 navbar_text <?php echo $food_class; ?>" href="../products/products.php">Food</a>
+                    <a class="nav-link mx-3 navbar_text <?php echo $food_class; ?>"
+                        href="../products/products.php">Food</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-3 navbar_text <?php echo $gallery_class; ?>" href="../products/gallery.php">Gallery</a>
+                    <a class="nav-link mx-3 navbar_text <?php echo $gallery_class; ?>"
+                        href="../products/gallery.php">Gallery</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-4 navbar_text <?php echo $login_class; ?>" href="../../views/products/My_saved_recipes.php">My Saved Recipes</a>
+                    <a class="nav-link mx-4 navbar_text <?php echo $login_class; ?>"
+                        href="../../views/products/My_saved_recipes.php">My Saved Recipes</a>
 
                 </li>
 
@@ -126,26 +131,27 @@ if ($user_id) {
             </form>
             <div id="search_results"></div>
             <div id="popupSearch" class="popup">
-    <div class="popup-content">
-        <span class="close" id="closePopup">&times;</span>
-        <form class="d-flex" role="search" style="width: 100%;">
-            <div class="input-group me-1">
-                <input class="form-control search-bar-popup" type="search" placeholder="Search" aria-label="Search" id="search-bar-popup">
-                <div class="input-group-text" style="border: 2px solid #ff8800 !important;">
-                    <button class="btn search-icon-popup"><i class="fas fa-search"></i></button>
+                <div class="popup-content">
+                    <span class="close" id="closePopup">&times;</span>
+                    <form class="d-flex" role="search" style="width: 100%;">
+                        <div class="input-group me-1">
+                            <input class="form-control search-bar-popup" type="search" placeholder="Search"
+                                aria-label="Search" id="search-bar-popup">
+                            <div class="input-group-text" style="border: 2px solid #ff8800 !important;">
+                                <button class="btn search-icon-popup"><i class="fas fa-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                    <div id="search-results-popup"></div>
                 </div>
             </div>
-        </form>
-        <div id="search-results-popup"></div>
-    </div>
-</div>
 
 
 
 
             <?php
             if (isset($_COOKIE["userID"])) {
-            ?>
+                ?>
                 <div class="dropdown">
                     <button class="btn  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fa-solid fa-user fs-4"></i>
@@ -153,9 +159,11 @@ if ($user_id) {
                     <ul class="dropdown-menu dropdownUser">
                         <li style="font-weight: bold;
                                     color: #ffa500;
-                                    padding-left: 10px ;">Hello : <?php echo $user_fullname; ?></li>
-                        <li><a class="dropdown-item" href="../home/user_profile.php">Profile</a></li>
-                        <li><a class="dropdown-item" href="../../models/user/logout.php">Logout</a></li>
+                                    padding-left: 10px ;"> <?php echo $user_fullname; ?></li>
+                        <li><a class="dropdown-item" href="../auth/user_profile.php"><i class="fa-solid fa-user"></i>
+                                Profile</a></li>
+                        <li><a class="dropdown-item" href="../../models/user/logout.php"><i
+                                    class="fa-solid fa-right-from-bracket"></i> Logout</a></li>
                     </ul>
                 </div>
             <?php } else { ?>
@@ -171,18 +179,18 @@ if ($user_id) {
 
 
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function () {
         const searchBar = document.getElementById('search_bar');
         const searchResults = document.getElementById('search_results');
         searchResults.style.display = 'none';
-        searchBar.addEventListener('input', function() {
+        searchBar.addEventListener('input', function () {
             const searchTerm = searchBar.value.trim(); // Get the value of the search input
             if (searchTerm !== '') {
                 // Send AJAX request to fetch search results
                 const xhr = new XMLHttpRequest();
                 xhr.open('POST', '../includes/search.php', true);
                 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                xhr.onreadystatechange = function() {
+                xhr.onreadystatechange = function () {
                     if (xhr.readyState == 4 && xhr.status == 200) {
                         // Handle the response and display search results
                         searchResults.innerHTML = xhr.responseText;
@@ -197,23 +205,23 @@ if ($user_id) {
             }
         });
     });
-    document.addEventListener("DOMContentLoaded", function() {
-    const searchBar = document.getElementById('search_bar');
-    const searchIcon = document.getElementById('search_icon');
+    document.addEventListener("DOMContentLoaded", function () {
+        const searchBar = document.getElementById('search_bar');
+        const searchIcon = document.getElementById('search_icon');
 
-    searchIcon.addEventListener('click', function(event) {
-        event.preventDefault(); 
+        searchIcon.addEventListener('click', function (event) {
+            event.preventDefault();
 
-        const searchTerm = searchBar.value.trim();
-        if (searchTerm !== '') {
-            window.location.href = '../products/products.php?search=' + encodeURIComponent(searchTerm);
-        } else {
-            window.location.href = '../products/products.php';
-        }
+            const searchTerm = searchBar.value.trim();
+            if (searchTerm !== '') {
+                window.location.href = '../products/products.php?search=' + encodeURIComponent(searchTerm);
+            } else {
+                window.location.href = '../products/products.php';
+            }
+        });
     });
-});
 
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         if (window.scrollY > 50) {
             document.querySelector('nav').classList.add('fixed-top');
             navbar_height = document.querySelector('nav').offsetHeight;
@@ -224,30 +232,30 @@ if ($user_id) {
         }
     });
     if (window.matchMedia("(max-width: 756px)").matches) {
-    document.getElementById("search_icon").id = "search_icon_mb";
-}
-window.addEventListener('resize', function() {
-    if (window.matchMedia("(max-width: 756px)").matches) {
         document.getElementById("search_icon").id = "search_icon_mb";
-    } else {
-        if (document.getElementById("search_icon_mb")) {
-            document.getElementById("search_icon_mb").id = "search_icon";
-        }
     }
-});
-// JavaScript for Popup Search
-document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener('resize', function () {
+        if (window.matchMedia("(max-width: 756px)").matches) {
+            document.getElementById("search_icon").id = "search_icon_mb";
+        } else {
+            if (document.getElementById("search_icon_mb")) {
+                document.getElementById("search_icon_mb").id = "search_icon";
+            }
+        }
+    });
+    // JavaScript for Popup Search
+    document.addEventListener("DOMContentLoaded", function () {
         const searchBarPopup = document.getElementById('search-bar-popup');
         const searchResultsPopup = document.getElementById('search-results-popup');
         searchResultsPopup.style.display = 'none';
-        searchBarPopup.addEventListener('input', function() {
+        searchBarPopup.addEventListener('input', function () {
             const searchTerm = searchBarPopup.value.trim(); // Get the value of the search input
             if (searchTerm !== '') {
                 // Send AJAX request to fetch search results
                 const xhr = new XMLHttpRequest();
                 xhr.open('POST', '../includes/search.php', true);
                 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                xhr.onreadystatechange = function() {
+                xhr.onreadystatechange = function () {
                     if (xhr.readyState == 4 && xhr.status == 200) {
                         // Handle the response and display search results
                         searchResultsPopup.innerHTML = xhr.responseText;
@@ -262,30 +270,30 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     });
-document.getElementById("search_icon_mb").addEventListener("click", function() {
-    event.preventDefault(); 
-    document.getElementById("popupSearch").style.display = "flex";
-});
-
-document.getElementById("closePopup").addEventListener("click", function() {
-    document.getElementById("popupSearch").style.display = "none";
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    const searchBarPopup = document.querySelector('.search-bar-popup');
-    const searchIconPopup = document.querySelector('.search-icon-popup');
-    const searchResultsPopup = document.querySelector('.search-results-popup');
-
-    searchIconPopup.addEventListener('click', function(event) {
+    document.getElementById("search_icon_mb").addEventListener("click", function () {
         event.preventDefault();
-        const searchTerm = searchBarPopup.value.trim();
-        if (searchTerm !== '') {
-            // Handle search action here, e.g., redirect to search results page
-            // Replace the following line with your desired action
-            window.location.href = '../products/products.php?search=' + encodeURIComponent(searchTerm);
-        }
+        document.getElementById("popupSearch").style.display = "flex";
     });
-});
+
+    document.getElementById("closePopup").addEventListener("click", function () {
+        document.getElementById("popupSearch").style.display = "none";
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const searchBarPopup = document.querySelector('.search-bar-popup');
+        const searchIconPopup = document.querySelector('.search-icon-popup');
+        const searchResultsPopup = document.querySelector('.search-results-popup');
+
+        searchIconPopup.addEventListener('click', function (event) {
+            event.preventDefault();
+            const searchTerm = searchBarPopup.value.trim();
+            if (searchTerm !== '') {
+                // Handle search action here, e.g., redirect to search results page
+                // Replace the following line with your desired action
+                window.location.href = '../products/products.php?search=' + encodeURIComponent(searchTerm);
+            }
+        });
+    });
 
 </script>
 
